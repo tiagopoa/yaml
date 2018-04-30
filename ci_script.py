a14a0280ab50b61b9feaa88cc3a7a82bdf444f63
Tiago Correa <tiagopoa@gmail.com>
2018-04-30 13:31:20 -0300

    Criada funçao pipeline_executor e incluido toda logica dentro desta função. Movido bloco main para o final do arquivo e inserida a chamada da função pipeline_executor() passando o argumento data

--- ci_script.py
+++ ci_script.py
@@ -7,32 +7,35 @@ def yaml_loader(filepath):
         data = yaml.load(file_descriptor)
     return data
 
+def pipeline_executor(data):
+    #Verifica quantos passos existem e armazena qual é a ordem de execução
+    x=0
+    passos={}
+    for i in data['steps'][0]['execute']:
+        passos[x]=data['steps'][0]['execute'][x]
+        x+=1
+
+    print("tasks j: ",data.get('tasks')[0])
+    #Procura as instruções e as executa na ordem correta.
+    for i in range(0,x):
+        for j in range(0,x):
+            if (str(data['tasks'][j]).find(passos[i])==-1):
+                print()
+            else:
+                cmd = (data.get('tasks')[j][passos[i]]['cmd'])
+                print("Iniciando execução do comando",cmd,"aguarde...")
+                print()            
+                try:
+                    output = subprocess.check_output(cmd, shell=False)                
+                except subprocess.CalledProcessError as e:
+                    print()
+                    print ("Erro:",e.output)
+                print()
+                print("Processo",cmd,"finalizado.")
+                print()
+
 if __name__ == "__main__":
     filepath = "pipeline.yml"
     data = yaml_loader(filepath)
-print()
+    pipeline_executor(data)
 
-#Verifica quantos passos existem e qual é a ordem de execução
-x=0
-passos={}
-for i in data['steps'][0]['execute']:
-    passos[x]=data['steps'][0]['execute'][x]
-    x+=1
-
-#Procura as instruções e as executa na ordem correta.
-for i in range(0,x):
-    for j in range(0,x):
-        if (str(data['tasks'][j]).find(passos[i])==-1):
-            print()
-        else:
-            cmd = (data.get('tasks')[j][passos[i]]['cmd'])
-            print("Iniciando execução do comando",cmd,"aguarde...")
-            print()            
-            try:
-                output = subprocess.check_output(cmd, shell=False)                
-            except subprocess.CalledProcessError as e:
-                print()
-                print ("Erro:",e.output)
-            print()
-            print("Processo",cmd,"finalizado.")
-            print()
