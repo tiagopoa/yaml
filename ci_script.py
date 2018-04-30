0a17799d92ef34230dbdba1946195680f66fae9a
Tiago Correa <tiagopoa@gmail.com>
2018-04-30 10:47:37 -0300

    FOR aninhados e melhorado acesso aos itens do arquivo YAML

--- ci_script.py
+++ ci_script.py
@@ -7,19 +7,9 @@ def yaml_loader(filepath):
         data = yaml.load(file_descriptor)
     return data
 
-#def yaml_dump(filepath, data):
-#    """Dumps data to a yaml file"""
-#    with open(filepath, "w") as file_descriptor:
-#        yaml.dump(data, file_descriptor)
-#        #print(data)
-
 if __name__ == "__main__":
     filepath = "pipeline.yml"
     data = yaml_loader(filepath)
-
-#data = yaml_loader(filepath)
-#print(data['steps'][0]['execute'][0])
-#print(data['tasks'][1])
 print()
 
 #Verifica quantos passos existem e qual é a ordem de execução
@@ -28,38 +18,15 @@ passos={}
 for i in data['steps'][0]['execute']:
     passos[x]=data['steps'][0]['execute'][x]
     x+=1
-    #print(passos)
 
-#Procura a primeira instrução nos passos e executa
+#Procura as instruções e as executa na ordem correta.
 for i in range(0,x):
-    if (str(data['tasks'][0]).find(passos[i]) ==-1):
-        print()
-    else:
-        print("executando:",data['tasks'][i])
-        cmd = str(data['tasks'][i])
-        #print(cmd[21:52])
-        r = subprocess.call(cmd[21:52], shell=True)
-        #proc = subprocess.Popen (cmd[21:52], shell=True,stdout=subprocess.PIPE).stdout.read()
-        print()
-
-#Procura a segunda instrução nos passos e executa
-for i in range(0,x):
-    if (str(data['tasks'][1]).find(passos[i]) ==-1):
-        print()
-    else:
-        print("executando:", data['tasks'][i])
-        cmd = str(data['tasks'][i])
-        #print(cmd[19:69])
-        r = subprocess.call(cmd[19:69], shell=True)
-        #proc = subprocess.Popen (cmd[19:69], shell=True,stdout=subprocess.PIPE).stdout.read()
-        print()
-
-
-   
-
-
-
-
-#print(yaml.dump(data.get('tasks'), default_flow_style=False))
-#print(yaml.dump(data.get('steps'), default_flow_style=False))
-    
+    for j in range(0,x):
+        if (str(data['tasks'][j]).find(passos[i])==-1):
+            print()
+        else:
+            cmd = (data.get('tasks')[j][passos[i]]['cmd'])
+            print("executando:",cmd)
+            print()
+            subprocess.call(cmd, shell=True)
+            print()
